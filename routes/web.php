526e2b688027cc330bd366fde0ec0d5df09e96cb
rifatul <nabil.nouk@gmail.com>
2026-03-20 20:46:26 +0800

    Halaman My Products

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,15 @@ Route::middleware('auth:customer')->group(function () {
         Route::get('/password', Account\Password\Index::class)->name('account.password');
     });
 });
+
+//route home
+Route::get('/', Web\Home\Index::class)->name('home');
+
+//route products index
+Route::get('/products', Web\Products\Index::class)->name('web.product.index');
+
+//route category show
+Route::get('/category/{slug}', Web\Category\Show::class)->name('web.category.show');
+
+//route product show
+Route::get('/products/{slug}', Web\Products\Show::class)->name('web.product.show');
